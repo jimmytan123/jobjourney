0e212a1283bc54d5770e46c86ba0b87f563d74d4
Jimmy Tan <jimmytan0424@gmail.com>
2024-08-18 16:46:40 -0700

    Convert pw to hashed version before saving in DB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 import { NotFoundError } from '../errors/customErrors.js';
+import bcrypt from 'bcryptjs';
 
 // @desc Register a new user
 // route POST /api/v1/auth/register
@@ -11,9 +12,18 @@ export const register = async (req, res, next) => {
     const usersCount = await User.countDocuments();
     req.body.role = usersCount === 0 ? 'admin' : 'user';
 
+    // Convert password into the hashed version
+    const salt = await bcrypt.genSalt(10);
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+
+    // Create user in DB
     const user = await User.create(req.body);
 
-    res.status(StatusCodes.CREATED).json({ user });
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: 'user created successfully', email: user.email });
   } catch (err) {
     next(err);
   }
