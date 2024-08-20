d8747d8265eb3233862261c069c9391ee96ba3dc
Jimmy Tan <jimmytan0424@gmail.com>
2024-08-20 14:51:27 -0700

    Set up proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // with options: http://localhost:5173/api/bar-> http://localhost:5100/api/bar
+      '/api': {
+        target: 'http://localhost:5100/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // to remove the api prefix
+      },
+    },
+  },
+});
