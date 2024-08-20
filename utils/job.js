b5258442c5f9713e23976d130b3042c194ab07eb
Jimmy Tan <jimmytan0424@gmail.com>
2024-08-20 10:10:38 -0700

    Update to use constant

--- utils/job.js
+++ utils/job.js
@@ -1,8 +1,10 @@
+import { USER_TYPE } from './constant.js';
+
 export const isAllowToRequest = (job, requestedUser) => {
   let isAllowed = true;
 
   // Check if the requested account is an admin
-  const isAdmin = requestedUser.role === 'admin';
+  const isAdmin = requestedUser.role === USER_TYPE.ADMIN;
   // Check if the requested account is the user who create the job
   const isJobOwner = requestedUser.userId === job.createdBy.toString();
 
