9ec61a8ab638ef15d8bbf184814b74b1aa503b3d
Jimmy Tan <jimmytan0424@gmail.com>
2024-09-03 11:02:42 -0700

    Update production install script

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Static folder configs (public folder)
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 // Parser configs
 app.use(express.json());
@@ -56,9 +56,9 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/admin', authenticateUser, adminRouter);
 
-// Set up path for Front-end entry point, the public/index.html coming from the FE build(dist) file
+// Set up path for Front-end entry point
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 // NOT FOUND Route
