eef0c0ab56deb0f463e95e036be7e17b353af96e
Jimmy Tan <jimmytan0424@gmail.com>
2024-09-04 10:05:33 -0700

    Remove console log

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -10,7 +10,7 @@ import {
 } from 'recharts';
 
 const BarChartComponent = ({ data }) => {
-  console.log(data);
+  // console.log(data);
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart
